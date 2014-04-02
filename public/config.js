e7c2f36c469ec0e65342cdd336c0353d436ad91e
Travis Collins <travis@tec27.com>
2014-04-01 23:22:14 -0700

    Fix dep versions, remove animated_gif dependency.

--- public/config.js
+++ public/config.js
@@ -2,7 +2,6 @@ requirejs.config({
   deps: ['main'],
   paths: {
     'jquery': 'lib/jquery/jquery',
-    'Animated_GIF': 'lib/animated-gif/dist/Animated_GIF.min',
     'fingerprint': 'lib/fingerprint/fingerprint',
     'md5': 'lib/js-md5/js/md5',
     'waypoints': 'lib/jquery-waypoints/waypoints',
